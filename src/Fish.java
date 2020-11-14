f0a45973f181976a7080851eea71637f277c258f
hiedwinhi <hiedwinhi@users.noreply.github.com>
2020-11-14 11:59:54 +0800

    delete the main

---
+++ src/Fish.java
@@ -0,0 +1,12 @@
+public class Fish {
+    public int size;
+    public int weight;
+    public void swim(){
+        System.out.println("I can swim");
+    }
+    public void eat(){
+        System.out.println("I can eat");
+    }
+
+
+}
